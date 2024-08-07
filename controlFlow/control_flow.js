f630d20fdd1062ca614b557b3f87f690c0aba0e9
udemeabasi <udemeabasi6@gmail.com>
2024-08-06 23:16:03 -0400

    The addition of control flow folder and writing of codes

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,50 @@
+// Declaration of variables (String)
+let userRole = "admin"
+let accessLevel;
+
+// If else statement
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+// Definition of Output
+console.log("Access Level:", accessLevel);
+
+// Declaration of variables (Boolean)
+let isLoggedIn = true;
+let userMessage;
+
+//Nested if... else statement
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+
+let userType = "subscriber"
+let userCategory;
+
+//Execution of switch statement
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
