4fc388c67fa207f2e0334c51a28b0848e2511296
udemeabasi <udemeabasi6@gmail.com>
2024-08-06 23:23:46 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -48,3 +48,10 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+//Use of ternary operator for isAuthenticated and authenticationStatus
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
